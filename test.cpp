f949820178f38fdbc0f733174162d74c5485e2b2
TimusTrun <timtom9801@gmail.com>
2020-10-09 18:19:44 -0700

    added 2 test cases

--- test.cpp
+++ test.cpp
@@ -11,6 +11,16 @@ TEST(EchoTest, EmptyString) {
 	EXPECT_EQ ("", echo(1,test_val));
 }
 
+TEST(EchoTest, ThomasHTran){
+	char* test_val[4]; test_val[0] = "./c-echo"; test_val[1] = "Thomas"; test_val[2] = "H"; test_val[3] = "Tran";
+	EXPECT_EQ ("Thomas H Tran", echo(4,test_val));
+}
+
+TEST(EchoTest, OneTwoThree){
+	char* test_val[4]; test_val[0] = "./c-echo"; test_val[1] = "1"; test_val[2] = "2"; test_val[3] = "3";
+	EXPECT_EQ ("1 2 3", echo(4,test_val));
+}
+
 int main(int argc, char **argv){
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
